80cb8c6f6a447fa78e8a4c158d0a14420470237f
Ryan Sheridan <0xryann@protonmail.com>
2025-10-30 19:41:10 +0000

    feat: mTLS, checkpoint

--- src/main/java/grouph/Server.java
+++ src/main/java/grouph/Server.java
@@ -31,6 +31,61 @@ public class Server {
         try {
             // TODO: figure out steps to complete and write code to complete them
             System.out.println("hello from server");
+
+            // load servers key store, which contains servers private key and cert
+            KeyStore keyStore = KeyStore.getInstance("JKS");
+            try(FileInputStream keyStoreFile = new FileInputStream(KEYSTORE_PATH)) {
+                keyStore.load(keyStoreFile, KEYSTORE_PASSWORD.toCharArray());
+            }
+            System.out.println("loaded server keystore");
+
+            // use KeyManager to present server cert to clients
+            KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+            kmf.init(keyStore, KEYSTORE_PASSWORD.toCharArray());
+            System.out.println("init key manager");
+
+            // load server truststore
+            KeyStore trustStore = KeyStore.getInstance("JKS");
+            try(FileInputStream trustStoreFile = new FileInputStream(TRUSTSTORE_PATH)) {
+                trustStore.load(trustStoreFile, TRUSTSTORE_PASSWORD.toCharArray());
+            }
+            System.out.println("loaded server trust store (trusted clients)");
+
+            // TrustManager validates incoming client certificates,
+            // is this client certificate signed by someone i trust?
+            TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+            tmf.init(trustStore);
+            System.out.println("init trustmanager (verify client certs)");
+
+            // main security config object (SSLContext)
+            // we combine both KeyManger and TrustManager
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(
+                kmf.getKeyManagers(),   // how to present our identity
+                tmf.getTrustManagers(), // how to verify client identity
+                new SecureRandom()      // rng for crypto
+            );
+            System.out.println("created SSLContext");
+
+            // here we create our SSLServerSocket, the important bit
+            SSLServerSocketFactory factory = sslContext.getServerSocketFactory();
+            serverSocket = (SSLServerSocket) factory.createServerSocket(PORT);
+
+            // the line that makes it "mutual TLS"
+            // without this line only the server would be authenticated (TLS not mTLS)
+            // with this both server and client must prove identity
+            serverSocket.setNeedClientAuth(true);
+
+            running = true;
+            System.out.printf( // programming java for 3 years and only know i realise printf is a thing
+                """
+                server started on port %d
+                waiting for client connection
+                """, PORT
+            );
+
+
+
         } catch (Exception e) {
             System.err.println("failed to start server: " + e.getMessage());
             e.printStackTrace();
