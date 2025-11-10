79314578d8f1cc49c02f24b29db90b9a86c3e4b0
Ryan Sheridan <0xryann@protonmail.com>
2025-11-10 14:25:54 +0000

    fix: all tests running

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,5 +27,8 @@ tasks.withType<JavaCompile>().configureEach {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform() // <--- enable JUnit 5
 }
