564d27f0b0a52d95fd35ec67b3ee7ab3259df1e1
Ryan Sheridan <0xryann@protonmail.com>
2025-11-10 14:23:08 +0000

    fix: something

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,10 @@
 plugins {
     java
+    application
+}
+
+application {
+    mainClass.set("grouph.Main")
 }
 
 group = "grouph"
@@ -23,8 +28,4 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-}
-
-tasks.test {
-    useJUnitPlatform()
 }
